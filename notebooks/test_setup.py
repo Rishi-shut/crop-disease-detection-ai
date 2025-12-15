bce96cfbbd42843377ac5eb2f66d6e875a135c26
Mrigank Singh <mriganksingh7890@gmail.com>
2025-12-15 19:40:04 +0530

    Initial commit: project setup and environment test

---
+++ notebooks/test_setup.py
@@ -0,0 +1,8 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+import streamlit
+
+print("TensorFlow version:", tf.__version__)
+print("OpenCV version:", cv2.__version__)
+print("ALL GOOD 🚀")
